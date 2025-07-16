cf57ab6464bf418f0ed7047bf67fb32a97284762
Vazcuus <123123Lev@>
2025-07-16 15:00:40 +0700

    Test work

---
+++ MSVC/linux-build-x64/CMakeFiles/SQLite.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SQLite.dir/src/sqlite3.c.o"
+  "CMakeFiles/SQLite.dir/src/sqlite3.c.o.d"
+  "lib/libSQLite_x64.pdb"
+  "lib/libSQLite_x64.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SQLite.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
